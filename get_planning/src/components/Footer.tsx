dc8d5367f50dad2df144e2c584638105a6f45f5b
alilat_y <alilat_y@etna-alternance.net>
2023-03-09 00:33:21 +0100

    style fix

--- get_planning/src/components/Footer.tsx
+++ get_planning/src/components/Footer.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import './Footer.css';
+import styles from '@/styles/Footer.module.css';
 
 function Footer() {
   return (
-    <div className="footer">
-      <div className="footer-links">
+    <footer className={styles.footer}>
+      <div className={styles["footer-links"]}>
         <a href="#">Link 1</a>
         <a href="#">Link 2</a>
         <a href="#">Link 3</a>
       </div>
-      <div className="footer-social">
+      <div className={styles["footer-social"]}>
         <a href="#">Facebook</a>
         <a href="#">Twitter</a>
         <a href="#">Instagram</a>
       </div>
-      <div className="footer-copy">
+      <div className={styles["footer-copy"]}>
         &copy; 2023 My Website. All rights reserved.
       </div>
-    </div>
+    </footer>
   );
 }
 
