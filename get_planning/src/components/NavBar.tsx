d82fc0703ab328daa1506724dd5eafac14faa44d
elhasn_a <elhasn_a@etna-alternance.net>
2023-03-09 10:49:31 +0100

    [add]top

--- get_planning/src/components/NavBar.tsx
+++ get_planning/src/components/NavBar.tsx
@@ -1,12 +1,40 @@
 import styles from "@/styles/Navbar.module.scss";
+import { useState, useEffect } from "react";
 
 function Navbar() {
+
+  const [atTop, setAtTop] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setAtTop(true);
+      } else {
+        setAtTop(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
   <div className={styles.navbar}>
     <img className={styles.logoimg} src="/navbar/calendrier.png"/>
     <div className={styles.logo_connexion} />
+    {!atTop && ( // appears when the scroll bar is not fully
+          <div>
+            <a href="#top">
+              
+              <img className={styles.top} src="/navbar/Midou.png"/>
+            </a>
+          </div>
+        )}
   </div>
   );
 }
+
 
 export default Navbar;
